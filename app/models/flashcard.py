38291aafdbca9e4ac913bf2bafbf75953be9e11e
Rachael <mcbride.rac@gmail.com>
2021-12-27 11:39:18 -0500

    Updates Flashcard model and spaced repetition algorithm

--- app/models/flashcard.py
+++ app/models/flashcard.py
@@ -1,3 +1,13 @@
+# Credit to https://github.com/thyagoluciano/sm2 for explanation of SM2, 
+# which the reset_values_based_on_sm2 method below is based on
+# Key for difficult level values:
+    # 5 - very easy 
+    # 4 - easy 
+    # 3 - medium 
+    # 2 - hard 
+    # 1 - very hard 
+    # 0 - complete blackout  
+
 from app import db
 from sqlalchemy.orm import backref
 from datetime import datetime, timedelta
@@ -7,32 +17,18 @@ class Flashcard(db.Model):
     front = db.Column(db.Text)
     back = db.Column(db.Text)
     deck_id = db.Column(db.Integer, db.ForeignKey("deck.id"))
-
-    # spaced repetition attributes 
-    difficulty_level = db.Column(db.Integer) # 0 to start then will update to user's choice
+    difficulty_level = db.Column(db.Integer) # 0 to start then will update to whatever user's choice is (on a scale of 0 to 5)
     previous_repetitions = db.Column(db.Integer) # 0 to start; will be incremented by 1 or reset to 0 based on user's choice
     previous_ease_factor = db.Column(db.Float) # 2.5 to start; will be recalculated based on user's choice
+    interval = db.Column(db.Integer) # 0 to start; will be recalculated based on user's choice; indicates the number of days to wait between reviews
+    date_to_review = db.Column(db.DateTime) # date/time of card creation to start; will be recalculated based on interval attribute (immediately above) 
 
-    # indicates the number of days to wait between reviews
-    interval = db.Column(db.Integer) # 0 to start; will be recalculated based on user's choice
-
-    # indicates when card should be reviewed again 
-    date_to_review = db.Column(db.DateTime) # date/time of card creation to start; will be recalculated based on user's choice
-
-    # DIFFICULTY LEVELS 
-    # 5 - very easy 
-    # 4 - easy 
-    # 3 - medium 
-    # 2 - hard 
-    # 1 - very hard 
-    # 0 - complete blackout  
-
-    # based on: https://github.com/thyagoluciano/sm2
     def reset_values_based_on_sm2(self, user_difficulty_level_choice):
         user_difficulty_level_choice = int(user_difficulty_level_choice)
 
-        # Reset interval 
+        # If user chooses "medium" to "very easy" as the card's level of difficulty...
         if user_difficulty_level_choice >= 3:
+            # Reset interval 
             if self.previous_repetitions == 0:
                 self.interval = 1 
             elif self.previous_repetitions == 1:
@@ -53,12 +49,12 @@ class Flashcard(db.Model):
             else:
                 self.previous_ease_factor = new_ease_factor
 
-        else: # (if self-reported difficulty level by user is < 3)
-            self.previous_repetitions = 0
-            self.interval = 1
+        else: # If user chooses "complete blackout" to "hard"...
+            self.previous_repetitions = 0 # reset reps to 0
+            self.interval = 1 # reset interval to 1
             # no change in ease factor 
 
-        # Reset date when card should be reviewed again 
+        # Reset date when card should be reviewed again based on `interval` attribute
         self.date_to_review = datetime.now() + timedelta(days=self.interval)
         self.difficulty_level = user_difficulty_level_choice
 
